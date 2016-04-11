46205da3176002f4282d483e9b360f10666f072b
pavel <birladeanupavel@gmail.com>
2016-04-11 22:32:44 +0300

    small changes

--- src/main/java/com/profiler/web/controller/rest/UserControllerRest.java
+++ src/main/java/com/profiler/web/controller/rest/UserControllerRest.java
@@ -2,6 +2,7 @@ package com.profiler.web.controller.rest;
 
 import com.profiler.dal.entity.User;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserControllerRest {
     
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @RequestMapping(value = "/user", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<User> getUser() {
         User user = new User();
         user.setFirstName("test");
